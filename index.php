95bdb6b20b7ba0dc90a4aa4c78147b7653fdfa3a
yurah25 <yusufrafiiahmad25@gmail.com>
2025-05-15 18:59:33 +0700

    kedua

--- index.php
+++ index.php
@@ -13,5 +13,6 @@ $nim = "H1D024049";
 </head>
 <body>
     <P> HALO NAMAKU <?php echo $name?></P>
+    <> Saya kuliah di UNSOED (Universitas Jenderal Soedirman)<br>Saya kuliah di Jurusan Informatika dengan Nim = <?php echo $nim?></p>
 </body>
 </html>
