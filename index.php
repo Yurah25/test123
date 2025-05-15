e1964a9365b85397bad2dcb3d2882daaa5cb8aac
yurah25 <yusufrafiiahmad25@gmail.com>
2025-05-15 18:53:27 +0700

    tambah nama

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+$name = "yusuf";
+$nim = "H1D024049";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <P> HALO NAMAKU <?php echo $name?></P>
+</body>
+</html>
