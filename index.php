7f251e87929191001765d417e465aa84f2185f1a
yurah25 <yusufrafiiahmad25@gmail.com>
2025-05-17 10:47:08 +0700

    test

--- index.php
+++ index.php
@@ -17,6 +17,7 @@ $nim = "H1D024049";
     <style>
     *,html{
          padding: 0;
+         margin: 0;
          font-family:'Montserrat', sans-serif ;
     }
 
@@ -25,12 +26,14 @@ $nim = "H1D024049";
         position:fixed;
         top: 0;
         left: 0;
+        height: 80px;
         z-index: 9999;
         padding-bottom: 5px;
         width: 100%;
         display: flex;
         justify-content: space-between;
         align-items: center; 
+        border-bottom: 1px solid #074f57;
      }   
     header h1{
         padding-left: 25px;
@@ -78,6 +81,41 @@ $nim = "H1D024049";
         width: 100%;
     }
     
+    .hero{
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+        background-image: url('../static/images/home.jpg');
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
+        position: relative;
+    }
+
+    .hero::after{
+        content: '';
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        display: block;
+        background: linear-gradient(deg 0%, rgba(78, 135, 250, ) 3%, rgba(255, 255, 255, 0) 25% )
+    }
+    .item{
+        color: rgb(255, 255, 255);
+        padding : 10rem;
+    }
+    .item h1{
+        font-weight: 700;
+        font-size: 5rem;
+        box-shadow: 1 1 3 1px black;
+    }
+    .tentang{
+        min-height: 100vh;
+        width: 100%;
+        display: flex;
+        background-color: rgb(78, 135, 250);
+        align-items: center;
+    }
     @media (max-width:1366px){
         html{
             font-size: 75%;
@@ -109,6 +147,16 @@ $nim = "H1D024049";
         </ul>
     </nav>
     </header>
+    <section class="hero" id="home">
+        <main class="item">
+            <h1>Selamat Datang</h1>
+            <p>Halo teman - teman ini adalah website percobaan yang saya buat<br>dan pertama kali yang saya buat dengan menghubungkan git didalamnya</p>
+        </main>
+    </section>
+    <section class="tentang" id="tentang">
+        <h1>Tentang Saya</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, et error voluptas nostrum iure aut<br>pariatur incidunt amet delectus possimus quae iusto numquam doloremque, doloribus excepturi harum molestias similique molestiae?</p>
+    </section>
     <script>
       feather.replace();
     </script>
